c63f4f379e1088cdfb497a98d67bcb81f456e2f0
Кирилл <fantom_youtube@inbox.ru>
2026-01-05 00:30:53 +0300

    style(task-card) : add delete button(doesn't work not)

---
+++ colloquim_1/colloquim_1.cpp
@@ -0,0 +1,12 @@
+﻿// colloquim_1.cpp: определяет точку входа для приложения.
+//
+
+#include "colloquim_1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
